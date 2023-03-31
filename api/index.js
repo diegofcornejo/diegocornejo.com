79769c24119340e6b86d89902650630523ddc58e
Diego Cornejo <diegof.cornejo@gmail.com>
2023-03-31 00:18:04 -0600

    Use env vars

--- api/index.js
+++ api/index.js
@@ -20,9 +20,9 @@ const handler = (req, res) => {
     const host = req.headers.host;
     const subdomain = host.split('.')[0];
     if (subdomain == 'meet') {
-        return res.redirect('https://meet.google.com/eyh-oehp-qsp')
+        return res.redirect(process.env.MEET_URL)
     }
-    return res.redirect('https://blog.diegocornejo.com');
+    return res.redirect(process.env.DEFAULT_URL);
 };
 
 module.exports = cors(handler);
