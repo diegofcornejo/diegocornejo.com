ed519a7c42fb3b53d4f604bf920c8422a7f3f6d3
Diego Cornejo <diegof.cornejo@gmail.com>
2023-11-03 18:34:03 -0600

    chore: Add Redis client and refactor subdomain redirection logic

--- api/index.js
+++ api/index.js
@@ -1,36 +1,38 @@
+import createRedisClient from "../utils/redis"
+
 const cors = fn => async (req, res) => {
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    // another common pattern
-    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    // res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS')
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
-    if (req.method === 'OPTIONS') {
-        res.status(200).end()
-        return
-    }
-    return await fn(req, res)
+	res.setHeader('Access-Control-Allow-Credentials', true)
+	res.setHeader('Access-Control-Allow-Origin', '*')
+	// another common pattern
+	// res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+	// res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+	res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS')
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+	)
+	if (req.method === 'OPTIONS') {
+		res.status(200).end()
+		return
+	}
+	return await fn(req, res)
 }
 
-const subdomains = {
-    blog: process.env.BLOG_URL,
-    meet: process.env.MEET_URL,
-    calendar: process.env.CALENDAR_URL,
+const handler = async (req, res) => {
+
+	let client;
+	let REDIRECT_URL = process.env.DEFAULT_URL;
+	try {
+		client = await createRedisClient();
+		const { host } = req.headers;
+		const subdomain = host.split('.')[0];
+		const REDIRECT_URL_VALUE = await client.get(`domain:${subdomain}`);
+		if (REDIRECT_URL_VALUE) REDIRECT_URL = REDIRECT_URL_VALUE;
+	} catch (error) {
+	} finally {
+		client.quit();
+		return res.redirect(REDIRECT_URL);
+	}
 };
 
-const handler = (req, res) => {
-    const { host } = req.headers;
-    const subdomain = host.split('.')[0];
-
-    if (subdomains[subdomain]) {
-        return res.redirect(subdomains[subdomain]);
-    }
-
-    return res.redirect(process.env.BLOG_URL);
-};
-
-module.exports = handler;
+export default handler;
---
+++ utils/redis.js
@@ -0,0 +1,25 @@
+import { createClient } from 'redis';
+
+async function createRedisClient() {
+  const client = createClient({ url: process.env.REDIS_URL });
+
+  return new Promise((resolve, reject) => {
+    client.on('error', (err) => {
+      console.error('Redis Client Error', err);
+      reject(err);
+    });
+
+    client.on('ready', () => {
+      console.log('Redis client connected');
+      resolve(client);
+    });
+
+		client.on('end', () => {
+			console.log('Redis client disconnected');
+		});
+
+    client.connect();
+  });
+}
+
+export default createRedisClient;
